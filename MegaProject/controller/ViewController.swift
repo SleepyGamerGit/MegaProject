761ffbe83a0492f491571dc8c5a8f21cd548ba14
Sleepy <hillsteadm@gmail.com>
2019-01-04 09:42:36 -0700

    made new project

---
+++ MegaProject/controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MegaProject
+//
+//  Created by Hillstead, Matthew on 1/4/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
